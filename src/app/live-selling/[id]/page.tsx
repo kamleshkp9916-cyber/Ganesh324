89b857cfa21c92e7426f400ff198f515fa786833
Ganesh Prajapati <kamleshkp9916@gmail.com>
2025-08-13 14:33:17 +0000

    more +5

--- src/app/live-selling/[id]/page.tsx
+++ src/app/live-selling/[id]/page.tsx
@@ -136,7 +136,7 @@ export default function LiveStreamPage({ params }: { params: { id: string } }) {
                     </div>
                 </div>
                
-                <div className="flex items-center gap-2">
+                <div className="flex items-center gap-2 -mr-5">
                     <Button 
                         size="icon" 
                         className={cn(
