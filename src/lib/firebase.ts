b31cf59c262d63d1680dcc9bcda5935efa689c4f
Ganesh Prajapati <kamleshkp9916@gmail.com>
2025-08-29 09:30:16 +0000

    modify the app so that firebase backend configuration is defined in .env

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,15 +1,15 @@
+
 import { initializeApp, getApps, getApp, FirebaseApp } from "firebase/app";
 import { getAuth, initializeAuth, browserLocalPersistence, Auth } from "firebase/auth";
 import { getFirestore, Firestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  "projectId": "streamcart-login",
-  "appId": "1:658712603017:web:61094aa63c361614659958",
-  "storageBucket": "streamcart-login.firebasestorage.app",
-  "apiKey": "AIzaSyD0gv_2NnS1zPrRE2rgF85fx61pyVLFKUs",
-  "authDomain": "streamcart-login.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "658712603017"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 let app: FirebaseApp;
