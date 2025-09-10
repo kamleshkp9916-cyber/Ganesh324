57ba445e5b5498e8b2fe590eb8674520cbb10ef4
Ganesh Prajapati <kamleshkp9916@gmail.com>
2025-09-10 10:38:33 +0000

    if youn see the background color of tabs are inconsistant

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -15,7 +15,7 @@ const TabsList = React.forwardRef<
   <TabsPrimitive.List
     ref={ref}
     className={cn(
-      "inline-flex h-auto items-center justify-center rounded-md bg-transparent p-1 text-primary-foreground",
+      "inline-flex h-auto items-center justify-center rounded-md bg-muted p-1 text-muted-foreground",
       className
     )}
     {...props}
