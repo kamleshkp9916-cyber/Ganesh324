e8c3888d482bca730039795c7ce690e21a222ccc
Ganesh Prajapati <kamleshkp9916@gmail.com>
2025-09-14 03:45:07 +0000

    now change this icon to shopping bag icon (_for element <Button>_)

--- src/app/listed-products/page.tsx
+++ src/app/listed-products/page.tsx
@@ -15,7 +15,7 @@ import {
 } from "@/components/ui/navigation-menu"
 import { Button } from "@/components/ui/button";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
-import { Menu, ShoppingBag, User, X, ChevronRight, ArrowLeft, Search } from "lucide-react";
+import { Menu, ShoppingBag, User, X, ChevronRight, ArrowLeft, Search, List } from "lucide-react";
 import { Logo } from "@/components/logo";
 import { useLocalStorage } from "@/hooks/use-local-storage";
 import { HUB_BANNER_KEY, HubBanner, HUB_FEATURED_PRODUCTS_KEY, FeaturedProduct } from '@/app/admin/settings/page';
@@ -258,3 +258,6 @@ const ListItem = React.forwardRef<
   )
 })
 ListItem.displayName = "ListItem"
+
+
+    
