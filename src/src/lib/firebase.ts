1292797fa45f473e91a77a1fb10e505e40523e09
Ganesh Prajapati <kamleshkp9916@gmail.com>
2025-09-17 11:20:41 +0000

    Try fixing this error: `Console Error: Each child in a list should have

--- src/src/lib/firebase.ts
+++ src/src/lib/firebase.ts
@@ -3,14 +3,16 @@ import { getStorage } from "firebase/storage";
 import { initializeFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "",
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "",
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "",
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "",
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "",
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "",
+  console.log("FIREBASE CONFIG (client):", {
+  apiKey: "AIzaSyD0gv_2NnS1zPrRE2rgF85fx61pyVLFKUs",
+  authDomain: "streamcart-login.firebaseapp.com",
+  databaseURL: "https://streamcart-login-default-rtdb.firebaseio.com",
+  projectId: "streamcart-login",
+  storageBucket: "streamcart-login.firebasestorage.app",
+  messagingSenderId: "658712603017",
+  appId: "1:658712603017:web:61094aa63c361614659958"
+  })
 };
-
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
 // ✅ Only experimentalForceLongPolling is valid in v10
