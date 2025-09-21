a3f75ccbcd03e427a1daa9297859e42ce2aeb952
Ganesh Prajapati <kamleshkp9916@gmail.com>
2025-09-21 03:22:25 +0000

    change this button icon to three lines  (_for element <Button>_)

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -4,7 +4,7 @@
 import * as React from "react"
 import { Slot } from "@radix-ui/react-slot"
 import { VariantProps, cva } from "class-variance-authority"
-import { PanelLeft } from "lucide-react"
+import { Menu } from "lucide-react"
 
 import { useIsMobile } from "@/hooks/use-mobile"
 import { cn } from "@/lib/utils"
@@ -282,7 +282,7 @@ const SidebarTrigger = React.forwardRef<
       }}
       {...props}
     >
-      {children || <PanelLeft />}
+      {children || <Menu />}
       <span className="sr-only">Toggle Sidebar</span>
     </Button>
   )
@@ -765,5 +765,3 @@ export {
   SidebarTrigger,
   useSidebar,
 }
-
-    
