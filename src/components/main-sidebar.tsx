a1776f92758b1b2427ca49b19210ff7ff0e94d83
Ganesh Prajapati <kamleshkp9916@gmail.com>
2025-09-21 03:14:45 +0000

    this back button should only appear in bigger screen so user can go back

--- src/components/main-sidebar.tsx
+++ src/components/main-sidebar.tsx
@@ -1,5 +1,4 @@
 
-
 "use client";
 
 import Link from 'next/link';
@@ -48,7 +47,7 @@ export function MainSidebar({ userData, userPosts }: MainSidebarProps) {
     return (
         <div className="p-6 flex flex-col h-full">
              <div className="flex items-center gap-2 mb-8">
-                 <Button variant="ghost" size="icon" className="-ml-2 md:hidden" onClick={() => router.push('/live-selling')}>
+                 <Button variant="ghost" size="icon" className="-ml-2 hidden md:inline-flex" onClick={() => router.push('/live-selling')}>
                     <ArrowLeft />
                 </Button>
                 <div className="flex-grow" />
