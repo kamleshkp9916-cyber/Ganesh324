d144b1451ad1e3c2d68b51ca240bc860640be6e4
Ganesh Prajapati <kamleshkp9916@gmail.com>
2025-10-16 08:08:18 +0000

    EmulatorConfigProvider.tsx:200   GET https://4000-firebase-studio-175497

---
+++ src/ai/dev.ts
@@ -0,0 +1,18 @@
+
+'use server';
+/**
+ * @fileoverview This is the development entrypoint for the Genkit service.
+ *
+ * It is not used in production.
+ */
+import { nextDev } from '@genkit-ai/next';
+import { defineDevApp } from 'genkit';
+
+export default defineDevApp({
+  plugins: [
+    nextDev({
+      appPath: '.',
+      watch: ['.'],
+    }),
+  ],
+});
