7d70ed186b8ff9b7cdc05a8f3d0de9115b87d762
Ganesh Prajapati <kamleshkp9916@gmail.com>
2025-10-18 09:48:29 +0000

    not worked try again

--- src/app/product/[productId]/page.tsx
+++ src/app/product/[productId]/page.tsx
@@ -1,10 +1,8 @@
 
-
 import { ProductDetailClient } from "@/components/product-detail-client";
 
 export default function ProductDetailPage({ params }: { params: { productId: string } }) {
-    return <ProductDetailClient productId={params.productId} />;
+    // Adding the key prop here is crucial. It tells React to create a new
+    // instance of the component when the productId changes, resetting its state.
+    return <ProductDetailClient key={params.productId} productId={params.productId} />;
 }
-
-
-    
