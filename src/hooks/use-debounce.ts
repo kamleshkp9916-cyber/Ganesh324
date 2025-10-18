43cdbbcd375c30b97b6d9355d19ac83c04201f04
Ganesh Prajapati <kamleshkp9916@gmail.com>
2025-10-18 07:25:46 +0000

    Try fixing this error: `Console Error: Maximum update depth exceeded. Th

--- src/hooks/use-debounce.ts
+++ src/hooks/use-debounce.ts
@@ -5,13 +5,18 @@ export function useDebounce<T>(value: T, delay?: number): T {
   const [debouncedValue, setDebouncedValue] = useState<T>(value);
 
   useEffect(() => {
-    const timer = setTimeout(() => setDebouncedValue(value), delay || 500);
+    // Update debounced value after delay
+    const handler = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay || 500);
 
+    // Cancel the timeout if value changes (also on delay change or unmount)
+    // This is how we prevent debounced value from updating if value is changed ...
+    // .. within the delay period. Timeout gets cleared and restarted.
     return () => {
-      clearTimeout(timer);
+      clearTimeout(handler);
     };
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [JSON.stringify(value), delay]);
+  }, [value, delay]);
 
   return debouncedValue;
 }
