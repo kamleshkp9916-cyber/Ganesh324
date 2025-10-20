71f89c4a67a0f804732963eede012766ed953bab
Ganesh Prajapati <kamleshkp9916@gmail.com>
2025-10-20 01:30:26 +0000

    i am not able to add to cart the different color and size of the same pr

--- src/components/product-detail-client.tsx
+++ src/components/product-detail-client.tsx
@@ -1,4 +1,5 @@
 
+
 "use client";
 
 import React, { useState, useEffect, useMemo, useRef, useCallback } from 'react';
@@ -1127,5 +1128,3 @@ export function ProductDetailClient({ productId }: { productId: string }) {
         </>
     );
 }
-
-    
