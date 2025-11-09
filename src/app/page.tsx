aa3697dff6302401169bf065dd6e44f966a7d4d9
Ganesh Prajapati <kamleshkp9916@gmail.com>
2025-11-09 08:18:27 +0000

    clicking this will should open that page  (_for element src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -63,7 +63,7 @@ export default function Home() {
             </div>
              <div>
                 Want to become a seller?{" "}
-                <Link href="/seller/kyc" className="font-semibold text-primary underline">
+                <Link href="/seller/register" className="font-semibold text-primary underline">
                   Register here
                 </Link>
             </div>
