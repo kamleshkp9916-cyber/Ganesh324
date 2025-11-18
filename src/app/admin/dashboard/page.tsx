863db8219d272f34825900d482e6076aa24671f2
Ganesh Prajapati <kamleshkp9916@gmail.com>
2025-11-18 03:01:48 +0000

    show this in inr (_for element <CardContent>_)

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -113,31 +113,31 @@ const recentSales = [
   {
     name: "Olivia Martin",
     email: "olivia.martin@email.com",
-    amount: "+$1,999.00",
+    amount: "+₹1,99,900",
     avatar: "https://placehold.co/40x40.png"
   },
   {
     name: "Jackson Lee",
     email: "jackson.lee@email.com",
-    amount: "+$39.00",
+    amount: "+₹3,900",
     avatar: "https://placehold.co/40x40.png"
   },
   {
     name: "Isabella Nguyen",
     email: "isabella.nguyen@email.com",
-    amount: "+$299.00",
+    amount: "+₹29,900",
     avatar: "https://placehold.co/40x40.png"
   },
   {
     name: "William Kim",
     email: "will@email.com",
-    amount: "+$99.00",
+    amount: "+₹9,900",
     avatar: "https://placehold.co/40x40.png"
   },
   {
     name: "Sofia Davis",
     email: "sofia.davis@email.com",
-    amount: "+$39.00",
+    amount: "+₹3,900",
     avatar: "https://placehold.co/40x40.png"
   },
 ]
@@ -371,5 +371,3 @@ export default function AdminDashboard() {
     </AdminLayout>
   )
 }
-
-    
