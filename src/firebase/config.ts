7a6bd9150243f3364199fa915653d2c521c83c71
Ganesh Prajapati <kamleshkp9916@gmail.com>
2025-11-20 16:38:08 +0000

    Try fixing this error: `Console Error: FirebaseError: Firebase Storage:

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,6 +1,8 @@
+
 export const firebaseConfig = {
   "projectId": "streamcart-login",
   "appId": "1:658712603017:web:61094aa63c361614659958",
+  "storageBucket": "streamcart-login.appspot.com",
   "apiKey": "AIzaSyD0gv_2NnS1zPrRE2rgF85fx61pyVLFKUs",
   "authDomain": "streamcart-login.firebaseapp.com",
   "measurementId": "",
