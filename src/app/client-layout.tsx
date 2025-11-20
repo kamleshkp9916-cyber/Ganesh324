622bf98144e594bbd56577a06f2fe34d34e9e3aa
Ganesh Prajapati <kamleshkp9916@gmail.com>
2025-11-20 14:59:04 +0000

    Try fixing this error: `Runtime Error: ChunkLoadError: Loading chunk app

--- src/app/client-layout.tsx
+++ src/app/client-layout.tsx
@@ -1,24 +1,19 @@
 'use client';
 
-import { AuthProvider } from '@/hooks/use-auth';
 import { AuthRedirector } from '@/components/auth-redirector';
 import { TopLoader } from '@/components/top-loader';
 import { Toaster } from '@/components/ui/toaster';
 import React from 'react';
-import { ImpersonationHandler } from '@/components/auth/impersonation-handler';
 import { MiniPlayerProvider } from '@/context/MiniPlayerContext';
 import { MiniPlayer } from '@/components/mini-player';
 
 export function ClientLayout({ children }: { children: React.ReactNode }) {
   return (
-    <AuthProvider>
-      <MiniPlayerProvider>
-        <ImpersonationHandler />
-        <AuthRedirector />
-        <React.Suspense fallback={<TopLoader />}>{children}</React.Suspense>
-        <MiniPlayer />
-        <Toaster />
-      </MiniPlayerProvider>
-    </AuthProvider>
+    <MiniPlayerProvider>
+      <AuthRedirector />
+      <React.Suspense fallback={<TopLoader />}>{children}</React.Suspense>
+      <MiniPlayer />
+      <Toaster />
+    </MiniPlayerProvider>
   );
 }
