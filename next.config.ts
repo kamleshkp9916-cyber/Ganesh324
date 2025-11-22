d01f4cf83e06a2fe28d5d7f69c77adda5868988f
Ganesh Prajapati <kamleshkp9916@gmail.com>
2025-11-22 12:14:42 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -53,6 +53,8 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
-      {
-        protocol: 'https',
-        hostname: 'api.
+    ],
+  },
+};
+
+export default nextConfig;
